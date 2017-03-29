729fd287d4b8d70829fc712c70ec68310ac02627
T.J. Tarazevits <tarazevits@gmail.com>
2017-03-28 21:27:06 -0400

    Removed non-functional api usage

--- src/app/actions/Episodes.js
+++ src/app/actions/Episodes.js
@@ -5,27 +5,6 @@ import http from 'http';
 import { Readable } from 'stream';
 import Immutable from 'immutable';
 
-export function fetchEpisodes2() {
-  return dispatch => {
-    dispatch({
-      type: ActionTypes.FETCH_EPISODES_ATTEMPTED,
-    });
-    RssApi.fetchEpisodes()
-    .then(data => {
-      const parser  = new FeedMe(true);
-      const s = new Readable();
-      s.push(data);
-      s.pipe(parser);
-      parser.on('end', function() {
-        dispatch({
-        type: ActionTypes.FETCH_EPISODES_SUCCEEDED,
-        data: Immutable.fromJS(parser.done()),
-        });
-      });
-    });
-  };
-}
-
 export function fetchEpisodes() {
   return dispatch => {
     dispatch({
